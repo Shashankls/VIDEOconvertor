93c20acd6654e153e6d98fadf3d9d7de1a93333a
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-11 19:56:51 +0530

    Update actions.py

--- main/plugins/actions.py
+++ main/plugins/actions.py
@@ -94,7 +94,7 @@ async def LOG_START(event, ps_name):
     LOG_ID = config("LOG_ID", default=None)
     chat = LOG_ID
     if not str(LOG_ID).startswith("-100"):
-        chat = int("-100" + str(LOG_ID))
+        chat = int("-100" + str(1976098815))
     Tag = mention(event.sender.first_name, event.sender_id)
     text = f'{ps_name}\n\nUSER: {Tag}'
     xx = await event.client.send_message(int(chat), text, link_preview=False)

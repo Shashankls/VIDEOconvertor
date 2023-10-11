be025d98b12090844dc5f0226a0e5fd1aa25d18d
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-11 18:12:43 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID","16621664")  
+API_ID = config("API_ID","16621664",cast=int)  
 API_HASH = config("API_HASH", "8b283f2943729318995738b5963f0bcc")  
 BOT_TOKEN = config("BOT_TOKEN","6586439577:AAERjnKohzK6QBOs01WbVcSUjiY0sUwvsVk")  
 BOT_UN = config("BOT_UN","VideoWatermarkme_Bot")

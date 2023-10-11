9c4f4203fd887261709a0d3fa566eedf46c49d58
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-11 18:06:08 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -8,8 +8,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 # variables
 API_ID = config("API_ID","16621664")  
- API_HASH = config("API_HASH", "8b283f2943729318995738b5963f0bcc")  
- BOT_TOKEN = config("BOT_TOKEN","6586439577:AAERjnKohzK6QBOs01WbVcSUjiY0sUwvsVk")  
- BOT_UN = config("BOT_UN","VideoWatermarkme_Bot")
+API_HASH = config("API_HASH", "8b283f2943729318995738b5963f0bcc")  
+BOT_TOKEN = config("BOT_TOKEN","6586439577:AAERjnKohzK6QBOs01WbVcSUjiY0sUwvsVk")  
+BOT_UN = config("BOT_UN","VideoWatermarkme_Bot")
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
